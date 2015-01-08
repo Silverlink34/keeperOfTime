bec4b68b2893ed9362d6f86dc4a084514c28ab6b
bryan31337 <bryan.jones31337@gmail.com>
2015-01-08 17:03:55 -0500

    init commit

---
+++ keeperOfTime/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace keeperOfTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.daCal = new System.Windows.Forms.MonthCalendar();
+            this.btnLookUp = new System.Windows.Forms.Button();
+            this.btnEndShift = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualEntryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.lblLunchEnd = new System.Windows.Forms.Label();
+            this.txtClockIn = new System.Windows.Forms.TextBox();
+            this.txtLunchIn = new System.Windows.Forms.TextBox();
+            this.txtLunchOut = new System.Windows.Forms.TextBox();
+            this.txtClockOut = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // daCal
+            // 
+            this.daCal.Location = new System.Drawing.Point(174, 33);
+            this.daCal.Name = "daCal";
+            this.daCal.TabIndex = 0;
+            this.toolTip.SetToolTip(this.daCal, "Select day then click \'Look Up\' to see shift info for that day.");
+            // 
+            // btnLookUp
+            // 
+            this.btnLookUp.Location = new System.Drawing.Point(326, 229);
+            this.btnLookUp.Name = "btnLookUp";
+            this.btnLookUp.Size = new System.Drawing.Size(75, 23);
+            this.btnLookUp.TabIndex = 1;
+            this.btnLookUp.Text = "Look Up";
+            this.btnLookUp.UseVisualStyleBackColor = true;
+            // 
+            // btnEndShift
+            // 
+            this.btnEndShift.Location = new System.Drawing.Point(12, 229);
+            this.btnEndShift.Name = "btnEndShift";
+            this.btnEndShift.Size = new System.Drawing.Size(75, 23);
+            this.btnEndShift.TabIndex = 2;
+            this.btnEndShift.Text = "End Shift";
+            this.toolTip.SetToolTip(this.btnEndShift, "End shift and save to spreadsheet.");
+            this.btnEndShift.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(419, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.manualEntryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // manualEntryToolStripMenuItem
+            // 
+            this.manualEntryToolStripMenuItem.Name = "manualEntryToolStripMenuItem";
+            this.manualEntryToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.manualEntryToolStripMenuItem.Text = "Manual entry";
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(12, 52);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 23);
+            this.btnIn.TabIndex = 4;
+            this.btnIn.Text = "Clock-in";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Location = new System.Drawing.Point(12, 81);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Size = new System.Drawing.Size(75, 23);
+            this.btnLunch.TabIndex = 5;
+            this.btnLunch.Text = "Lunch";
+            this.btnLunch.UseVisualStyleBackColor = true;
+            this.btnLunch.Click += new System.EventHandler(this.btnLunch_Click);
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(12, 139);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(75, 23);
+            this.btnOut.TabIndex = 7;
+            this.btnOut.Text = "Clock-out";
+            this.btnOut.UseVisualStyleBackColor = true;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(112, 167);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(50, 23);
+            this.btnDone.TabIndex = 8;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // lblLunchEnd
+            // 
+            this.lblLunchEnd.AutoSize = true;
+            this.lblLunchEnd.Location = new System.Drawing.Point(28, 115);
+            this.lblLunchEnd.Name = "lblLunchEnd";
+            this.lblLunchEnd.Size = new System.Drawing.Size(59, 13);
+            this.lblLunchEnd.TabIndex = 9;
+            this.lblLunchEnd.Text = "Lunch End";
+            // 
+            // txtClockIn
+            // 
+            this.txtClockIn.Location = new System.Drawing.Point(93, 54);
+            this.txtClockIn.Name = "txtClockIn";
+            this.txtClockIn.ReadOnly = true;
+            this.txtClockIn.Size = new System.Drawing.Size(69, 20);
+            this.txtClockIn.TabIndex = 10;
+            this.txtClockIn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLunchIn
+            // 
+            this.txtLunchIn.Location = new System.Drawing.Point(93, 83);
+            this.txtLunchIn.Name = "txtLunchIn";
+            this.txtLunchIn.ReadOnly = true;
+            this.txtLunchIn.Size = new System.Drawing.Size(69, 20);
+            this.txtLunchIn.TabIndex = 11;
+            this.txtLunchIn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLunchOut
+            // 
+            this.txtLunchOut.Location = new System.Drawing.Point(93, 112);
+            this.txtLunchOut.Name = "txtLunchOut";
+            this.txtLunchOut.ReadOnly = true;
+            this.txtLunchOut.Size = new System.Drawing.Size(69, 20);
+            this.txtLunchOut.TabIndex = 12;
+            this.txtLunchOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtClockOut
+            // 
+            this.txtClockOut.Location = new System.Drawing.Point(93, 141);
+            this.txtClockOut.Name = "txtClockOut";
+            this.txtClockOut.ReadOnly = true;
+            this.txtClockOut.Size = new System.Drawing.Size(69, 20);
+            this.txtClockOut.TabIndex = 13;
+            this.txtClockOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Hours for today:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 264);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClockOut);
+            this.Controls.Add(this.txtLunchOut);
+            this.Controls.Add(this.txtLunchIn);
+            this.Controls.Add(this.txtClockIn);
+            this.Controls.Add(this.lblLunchEnd);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.btnLunch);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.btnEndShift);
+            this.Controls.Add(this.btnLookUp);
+            this.Controls.Add(this.daCal);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Keeper Of Time";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar daCal;
+        private System.Windows.Forms.Button btnLookUp;
+        private System.Windows.Forms.Button btnEndShift;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualEntryToolStripMenuItem;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Label lblLunchEnd;
+        private System.Windows.Forms.TextBox txtClockIn;
+        private System.Windows.Forms.TextBox txtLunchIn;
+        private System.Windows.Forms.TextBox txtLunchOut;
+        private System.Windows.Forms.TextBox txtClockOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ keeperOfTime/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace keeperOfTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            string clockInTime = DateTime.Now.ToString("hh:mm tt");
+            txtClockIn.Text = clockInTime;
+            btnIn.Enabled = false;
+        }
+
+        private void btnLunch_Click(object sender, EventArgs e)
+        {
+            string lunchIn = DateTime.Now.ToString("hh:mm tt");
+            txtLunchIn.Text = lunchIn;
+
+            string lunchOut = DateTime.Now.AddHours(1).ToString("hh:mm tt");
+            txtLunchOut.Text = lunchOut;
+
+        }
+    }
+}
